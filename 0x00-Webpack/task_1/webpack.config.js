52a04880ca10e076aa997f966ecce1888f74ad54
Olartbaraq <akanbi.ges201@gmail.com>
2023-05-08 13:07:40 -0700

    task_1 webpack commit files

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js'),
+      },
+      output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'public')
+      },
+
+    plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Output",
+    }),
+  ],
+};
